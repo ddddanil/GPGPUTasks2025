2073c5b72e231757a7da71d5fb57f6c0e86ae525
Danil Doroshin <ddddanil5555@gmail.com>
2025-12-23 17:38:37 +0300

    feat(task,transpose02): impl local memory transpose

--- src/kernels/cu/matrix_02_transpose_coalesced_via_local_memory.cu
+++ src/kernels/cu/matrix_02_transpose_coalesced_via_local_memory.cu
@@ -7,13 +7,57 @@
 #include "helpers/rassert.cu"
 #include "../defines.h"
 
+__device__ uint shift_local(uint ix) {
+    uint row = ix / GROUP_SIZE_X;
+    uint col = ix % GROUP_SIZE_X;
+    uint shifted_col = (col + row) % GROUP_SIZE_X;
+    uint shifted_ix = row * GROUP_SIZE_X + shifted_col;
+    return shifted_ix;
+}
+
 __global__ void matrix_transpose_coalesced_via_local_memory(
                        const float* matrix,            // w x h
                              float* transposed_matrix, // h x w
                              unsigned int w,
                              unsigned int h)
 {
-    // TODO
+    const uint x_index = blockIdx.x * blockDim.x + threadIdx.x;
+    const uint y_index = blockIdx.y * blockDim.y + threadIdx.y;
+    const uint x_local = threadIdx.x;
+    const uint y_local = threadIdx.y;
+     __shared__ float local_buf[GROUP_SIZE_X * GROUP_SIZE_Y];
+
+    if (x_index >= w || y_index >= h)  {
+        return;
+    }
+
+    const uint src_ix = y_index * w + x_index;
+    const uint src_local_ix = y_local * GROUP_SIZE_Y + x_local;
+    const uint sh_src_local_ix = shift_local(src_local_ix);
+
+    const uint dst_row = (blockIdx.x * blockDim.x) + y_local;
+    const uint dst_col = (blockIdx.y * blockDim.y) + x_local;
+    const uint dst_ix = dst_row * h + dst_col;
+    const uint dst_local_ix = x_local * GROUP_SIZE_X + y_local;
+    const uint sh_dst_local_ix = shift_local(dst_local_ix);
+
+    // local_buf[src_local_ix] = matrix[src_ix];
+    // __syncthreads();
+    // transposed_matrix[dst_ix] = local_buf[dst_local_ix];
+    local_buf[sh_src_local_ix] = matrix[src_ix];
+    __syncthreads();
+    transposed_matrix[dst_ix] = local_buf[sh_dst_local_ix];
+
+    // if (x_index == 1 && y_index == 0) {
+    //     printf("worker (%d, %d):\n\tmove %d(%d,%d)->%d(%d,%d);\n\tbuf %d->%d, %d->%d\n\tval: %f <-> %f\n\tbuf val: %f <-> %f\n",
+    //            x_local, y_local,
+    //            src_ix, x_index, y_index, dst_ix, dst_col, dst_row,
+    //            src_local_ix, dst_local_ix, src_local_ix, dst_local_ix,
+    //            matrix[src_ix], transposed_matrix[dst_ix],
+    //            local_buf[src_local_ix], local_buf[dst_local_ix]
+    //     );
+    // }
+
 }
 
 namespace cuda {
